d8fbd611899e319d622e1610aea598abd90694d5
Jason Wai-Ho Lo <jasonalo40@gmail.com>
2017-07-18 16:09:51 -0400

    Included CSRF protection in static/javascripts/thinkster.js.

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -14,3 +14,18 @@
   angular
     .module('thinkster.routes', ['ngRoute']);
 })();
+
+angular
+  .module('thinkster')
+  .run(run);
+
+run.$inject = ['$http'];
+
+/**
+* @name run
+* @desc Update xsrf $http headers to align with Django's defaults
+*/
+function run($http) {
+  $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+  $http.defaults.xsrfCookieName = 'csrftoken';
+}
