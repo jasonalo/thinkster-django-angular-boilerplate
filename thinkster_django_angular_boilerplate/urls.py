99314eb06c0136dca68b4a9dd2119103f91e6915
Jason Wai-Ho Lo <jasonalo40@gmail.com>
2017-07-20 17:13:53 -0400

    Added import in url.py, included .js files in javascripts.html.

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url, include
 from thinkster_django_angular_boilerplate.views import IndexView
 from rest_framework_nested import routers
-from authentication.views import AccountViewSet, LoginView
+from authentication.views import AccountViewSet, LoginView, LogoutView
 
 
 router = routers.SimpleRouter()
